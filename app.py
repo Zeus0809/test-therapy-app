09ed77c03be609839bc161882238de7dfad6caa4
Zeus0809 <ikkit2002@gmail.com>
2025-06-27 15:09:57 -0400

    Created a database model for Patients.

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///patients.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Initialize the app with the database
+db.init_app(app)
+
+### Backend Logic
+
+### /Backend Logic
+
+# Create the database tables if they don't exist
+with app.app_context():
+    db.create_all()
 
 def main():
     print("Hello from test-therapy-app!")
-
+    app.run(debug=True)
 
 if __name__ == "__main__":
     main()
--- models.py
+++ models.py
@@ -1,5 +1,21 @@
 # Database models (python classes) to map to SQLite tables
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
 
+db = SQLAlchemy()
+
+class Patient(db.Model):
+
+    _id = db.Column("patient_id", db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    therapist_name = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, fname, lname, dob, therapist):
+        self.first_name = fname
+        self.last_name = lname
+        self.date_of_birth = dob
+        self.therapist_name = therapist
 
 
